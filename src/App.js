7065a5de4f7050bcb0365623de3e2094f4dde6df
Nicolas Machado <nicoma63@gmail.com>
2017-04-21 12:21:58 +0200

    Removed side menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import './css/main.min.css';
-import SideMenu from './components/side-menu';
 import GameHeader from './components/game-header';
 import GameBody from './components/game-body';
 
@@ -9,7 +8,6 @@ export class App extends React.Component {
     render() {
             return (
                 <div className="App">
-                    <SideMenu />
                     <div className="game-container">
                         <GameHeader />
                         <GameBody />
