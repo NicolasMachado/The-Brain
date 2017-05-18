acc20e06da200e572e17d78a413fda652b7c3649
Nicolas Machado <nicoma63@gmail.com>
2017-05-18 11:08:00 +0200

    changed favicon

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {connect} from 'react-redux';
 import './css/main.min.css';
 import GameHeader from './components/game-header';
 import GameBody from './components/game-body';
+import Favicon from 'react-favicon';
 
 export class App extends React.Component {
     render() {
             return (
                 <div className="App">
+                    <Favicon url={[require('./favicon.ico')]}/>
                     <div className="game-container">
                         <GameHeader />
                         <GameBody />
